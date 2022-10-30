5373a276aa2cc6ac68e5e1d32e0bb6c617568300
lukepian <116970632+lukepian@users.noreply.github.com>
2022-10-30 13:29:50 -0300

    Update candidato.py

--- entidade/candidato.py
+++ entidade/candidato.py
@@ -5,24 +5,9 @@ class Candidato(Eleitor):
     def __init__(self, nome: str, cpf: str, tipo_eleitor,
                  numero, chapa, cargo):
         super().__init__(nome, cpf, tipo_eleitor)
-        self.__nome = nome
-        self.__cpf = cpf
-        self.__tipo_eleitor = tipo_eleitor
         self.__numero = numero
         self.__chapa = chapa
         self.__cargo = cargo
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def cpf(self):
-        return self.__cpf
-
-    @property
-    def tipo_eleitor(self):
-        return self.__tipo_eleitor
 
     @property
     def numero(self):
@@ -35,18 +20,6 @@ class Candidato(Eleitor):
     @property
     def cargo(self):
         return self.__cargo
-
-    @nome.setter
-    def nome(self, nome: str):
-        self.__nome = nome
-
-    @cpf.setter
-    def cpf(self, cpf: int):
-        self.__cpf = cpf
-
-    @tipo_eleitor.setter
-    def tipo_eleitor(self, tipo_eleitor):
-        self.__tipo_eleitor = tipo_eleitor
 
     @numero.setter
     def numero(self, numero):
