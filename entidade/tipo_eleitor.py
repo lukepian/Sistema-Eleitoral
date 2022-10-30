fad2dd980e5981fcb40a9337d4061a9466b60013
lukepian <116970632+lukepian@users.noreply.github.com>
2022-10-30 05:34:04 -0300

    Update tipo_eleitor.py

--- entidade/tipo_eleitor.py
+++ entidade/tipo_eleitor.py
@@ -3,11 +3,16 @@
 class TipoEleitor:
     def __init__(self, nome: str):
         self.__nome = nome
+        self.__total_eleitores = 0
         self.__eleitores = []
 
     @property
     def nome(self):
         return self.__nome
+
+    @property
+    def total_eleitores(self):
+        return self.__total_eleitores
 
     @property
     def eleitores(self):
@@ -20,11 +25,9 @@ class TipoEleitor:
     def incluir_eleitor(self, eleitor):
         if eleitor not in self.__eleitores:
             self.__eleitores.append(eleitor)
+            self.__total_eleitores += 1
 
     def excluir_eleitor(self, eleitor):
         if eleitor in self.__eleitores:
             self.__eleitores.remove(eleitor)
-
-    def total_eleitores(self):
-        total = len(self.__eleitores)
-        return total
+            self.__total_eleitores -= 1
